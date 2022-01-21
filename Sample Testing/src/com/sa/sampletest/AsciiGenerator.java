ec2242d4f6c0f1d765957c2e5304b465e539491d
Santhosh <santhoshkumars.cs@gmail.com>
2022-01-21 23:18:01 +0530

    initial commit

---
+++ Sample Testing/src/com/sa/sampletest/AsciiGenerator.java
@@ -0,0 +1,11 @@
+package com.sa.sampletest;
+
+public class AsciiGenerator {
+	public static void main(String[] args) {
+		for (int i = 0; i < 128; i++) {
+			System.out.println(i + " " +(char) i);	
+		}
+
+	}
+
+}
